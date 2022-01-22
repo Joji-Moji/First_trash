e432ace245bdacfb356f563af93b443c92397ed8
Ypypy <kkkmit@mail.ru>
2022-01-22 12:19:02 +0300

    true

---
+++ app/src/main/java/com/example/myfirsttrashproj/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfirsttrashproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tr = 1
+    }
+}
