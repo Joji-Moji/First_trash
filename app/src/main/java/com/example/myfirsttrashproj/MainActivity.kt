7f855cc0c5c79a5a6927942ddfde9c26b324db16
Ypypy <kkkmit@mail.ru>
2022-01-22 16:57:25 +0300

    fun GetResult

--- app/src/main/java/com/example/myfirsttrashproj/MainActivity.kt
+++ app/src/main/java/com/example/myfirsttrashproj/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.myfirsttrashproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myfirsttrashproj.databinding.ActivityMainBinding
+
+
+private lateinit var  binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tr = 1
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    fun onClickResult (view: View){
+        if (!isFieldEmpty()){
+
+        }
+    }
+    private fun isFieldEmpty(): Boolean { // проверка на наличие введенных значений
+        binding.apply {
+            val b = R.string.nullEmpty.toString()
+           if(edA.text.isNullOrEmpty()) edA.error = b
+           if(edB.text.isNullOrEmpty()) edB.error = b
+            return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+
+        }
+    }
+    private fun getResult(): String{
+        val a: Double // переводим для использования уровнений
+        val b: Double
+        binding.apply {
+             a = edA.text.toString().toDouble()
+             b = edB.text.toString().toDouble()
+        }
+        return
+    }
+
 }
